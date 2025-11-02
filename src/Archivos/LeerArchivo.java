69f2aae32a2a59821a9b0fae82947d2b58317081
Sofia Uribe <souribeg@unal.edu.co>
2025-11-01 21:19:32 -0500

    commit inicial

---
+++ src/Archivos/LeerArchivo.java
@@ -0,0 +1,28 @@
+package Archivos;
+import java.io.*;
+
+public class LeerArchivo {
+    public static void main(String[] args) throws Exception{
+        String nombreArchivo = "C:/prueba.txt";
+        FileInputStream archivo;
+        InputStreamReader conversor;
+        BufferedReader filtro;
+        String linea;
+
+        try{
+            archivo = new FileInputStream(nombreArchivo);
+            conversor = new InputStreamReader(archivo);
+            filtro = new BufferedReader(conversor);
+            linea = filtro.readLine();
+            while (linea != null) {
+            System.out.println(linea);
+            linea = filtro.readLine();
+            }
+            filtro.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("No se pudo leer el archivo.");
+        }
+    }
+}
